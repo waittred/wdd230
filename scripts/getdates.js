b355128c9d3eb79d74fd9ba19eba2e95705d6257
waittred <boy15009@byui.edu>
2023-09-27 13:42:46 -0700

    lesson3 add hamburger menu

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,16 @@
+//To get the last updated info:
+
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
 let text = "Last Updated: " + document.lastModified;
     document.getElementById("date").innerHTML = text;
+
+//Lesson3 hamburger menu:
+
+const navigation = document.querySelector('.main-nav');
+const hamButton = document.querySelector('#menu');
+    
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('show');
+    hamButton.classList.toggle('show');
+    });  
