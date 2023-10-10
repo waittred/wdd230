c1838958ba50071f531c46c28d54b6efc90f1349
waittred <boy15009@byui.edu>
2023-10-09 19:20:35 -0700

    edit lesson05

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -1,5 +1,5 @@
 const input = document.getElementById('favchap');
-const button = document.getElementById('addButton');
+const button = document.getElementById('submit');
 const list = document.getElementById('list');
 
 button.addEventListener('click', () => {
