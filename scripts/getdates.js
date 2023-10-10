5878d370f778eb9b205701c8389ac55750a5742d
waittred <boy15009@byui.edu>
2023-10-09 21:32:36 -0700

    edit weather card

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -14,3 +14,25 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('show');
     hamButton.classList.toggle('show');
     });  
+ 
+//  Received help from chat.openai.com to fetch current weather //
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=47.60&lon=-122.33&units=imperial&appid=542a8196d8643c064f9c22f4313b339f'
+const currentTemp = document.getElementById('current-temp');
+const weatherIcon = document.getElementById('weather-icon');
+const captionDesc = document.getElementById('figcaption');
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const temperature = data.main.temp;
+    const iconCode = data.weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
+    const description = data.weather[0].description;
+
+    currentTemp.textContent = `Current Temperature: ${temperature} °F`;
+    weatherIcon.innerHTML = `<img src="${iconUrl}" alt="Weather Icon">`;
+    captionDesc.textContent = `Weather: ${description}`;
+  })
+  .catch(error => {
+    console.error('Error fetching weather data:', error);
+  });
