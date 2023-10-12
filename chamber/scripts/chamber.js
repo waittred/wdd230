7109bae1a28c6de30f7e930e4b2e574ce6e43906
waittred <boy15009@byui.edu>
2023-10-11 19:54:34 -0700

    Edit dark mode button

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -41,4 +41,13 @@ fetch(url)
 function myFunction() {
   let element = document.body;
   element.classList.toggle("dark-mode");
-}
+   // Get the button element
+   let button = document.querySelector('button');
+
+   // Enable or disable dark mode on or off//
+   if (element.classList.contains("dark-mode")) {
+     button.textContent = "Dark Mode On";
+   } else {
+     button.textContent = "Dark Mode Off";
+   }
+ }
