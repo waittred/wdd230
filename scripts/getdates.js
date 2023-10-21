a5b5068e1d70685665d72450f2067823426ac75c
waittred <boy15009@byui.edu>
2023-10-21 14:28:32 -0700

    fix layout for contact us and weather container font

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -10,31 +10,31 @@ let text = "Last Updated: " + document.lastModified;
 const navigation = document.querySelector('.main-nav');
 const hamButton = document.querySelector('#menu');
     
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('show');
-    hamButton.classList.toggle('show');
-    });  
- 
-
 ///  Received help from chat.openai.com to fetch current weather //
-const apiKey = '59020284cd6ab79b85c528f26efcafd2'; 
-        const city = 'Seattle'; 
+document.addEventListener('DOMContentLoaded', () => {
+    const apiKey = '59020284cd6ab79b85c528f26efcafd2';
+    const city = 'Seattle';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
-        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const temperatureCelsius = data.main.temp;
+            const temperatureFahrenheit = (temperatureCelsius * 9/5) + 32;
+            const windSpeedMph = data.wind.speed;
+            const weatherDescription = data.weather[0].description;
+            const weatherIcon = data.weather[0].icon;
 
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                const temperatureCelsius = data.main.temp;
-                const temperatureFahrenheit = (temperatureCelsius * 9/5) + 32; // Convert Celsius to Fahrenheit
-                const windSpeedMph = data.wind.speed;
-                const weatherDescription = data.weather[0].description;
+            document.getElementById('current-temperature').textContent = `Current Temperature: ${temperatureFahrenheit.toFixed(2)}°F`;
+            document.getElementById('wind-speed').textContent = `Wind Speed: ${windSpeedMph} mph`;
+            document.getElementById('weather-description').textContent = `Weather: ${weatherDescription}`;
 
-                // Update HTML elements with weather data
-                document.getElementById('current-temperature').textContent = `Current Temperature: ${temperatureFahrenheit.toFixed(2)}°F`;
-                document.getElementById('wind-speed').textContent = `Wind Speed: ${windSpeedMph} mph`;
-                document.getElementById('weather-description').textContent = `Weather: ${weatherDescription}`;
-            })
-            .catch(error => {
-                console.error('Error fetching weather data:', error);
-            });
+            const iconUrl = `https://openweathermap.org/img/w/${weatherIcon}.png`;
+            const weatherIconElement = document.getElementById('weather-icon');
+            weatherIconElement.setAttribute('src', iconUrl);
+            weatherIconElement.setAttribute('alt', 'Weather Icon');
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+});
