1969a36c6c6994b6a7b0daff87c5319bd843e80e
waittred <boy15009@byui.edu>
2023-10-24 21:39:33 -0700

    edited media screen, header, added dark mode

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -9,6 +9,26 @@ let text = "Last Updated: " + document.lastModified;
 
 const navigation = document.querySelector('.main-nav');
 const hamButton = document.querySelector('#menu');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('show');
+    hamButton.classList.toggle('show');
+    });  
+
+//Dark Mode//
+function myFunction() {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+     // Get the button element
+     let button = document.querySelector('button');
+  
+     // Enable or disable dark mode on or off//
+     if (element.classList.contains("dark-mode")) {
+       button.textContent = "Dark Mode On";
+     } else {
+       button.textContent = "Dark Mode Off";
+     }
+   }
     
 ///  Received help from chat.openai.com to fetch current weather //
 document.addEventListener('DOMContentLoaded', () => {
