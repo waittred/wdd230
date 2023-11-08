47f29b9cf45fe6707f5a157f9d1bd4c93090151b
waittred <boy15009@byui.edu>
2023-11-08 15:20:35 -0800

    lesson09 activities and edit chamber css pg sizes

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,41 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProphetData(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        displayProphets(data.prophets);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let birthdate = document.createElement('p');
+        let birthplace = document.createElement('p');
+        let portrait = document.createElement('img');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(birthdate);
+        card.appendChild(birthplace);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+}
+
+getProphetData(url);
+
