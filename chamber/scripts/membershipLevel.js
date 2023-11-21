a3a5f60e2b1c8f72af0145ac2383cf7f5102ed61
waittred <boy15009@byui.edu>
2023-11-21 15:39:28 -0800

    chamber index edits spotlight cards

--- chamber/scripts/membershipLevel.js
+++ chamber/scripts/membershipLevel.js
@@ -52,7 +52,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Append elements to the card
         card.appendChild(name);
-        card.appendChild(address);
         card.appendChild(img);
         card.appendChild(website);
 
