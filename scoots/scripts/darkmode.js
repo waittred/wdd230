60ee159ea219838b6206f8cc662f9960312f14b2
waittred <boy15009@byui.edu>
2023-11-29 21:55:44 -0800

    scooter setting up html pages, favicon, meta tags

---
+++ scoots/scripts/darkmode.js
@@ -0,0 +1,14 @@
+//Dark Mode//
+function myFunction() {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+     // Get the button element
+     let button = document.querySelector('button');
+  
+     // Enable or disable dark mode on or off//
+     if (element.classList.contains("dark-mode")) {
+       button.textContent = "Dark Mode On";
+     } else {
+       button.textContent = "Dark Mode Off";
+     }
+   }
