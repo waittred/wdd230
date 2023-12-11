f496b1ae5a8ac803fd315a29d6cb4dab6b03f537
waittred <boy15009@byui.edu>
2023-12-11 00:25:17 -0800

    scoots edit dark mode & alert buttons

--- scoots/scripts/darkmode.js
+++ scoots/scripts/darkmode.js
@@ -3,7 +3,7 @@ function myFunction() {
     let element = document.body;
     element.classList.toggle("dark-mode");
      // Get the button element
-     let button = document.querySelector('button');
+     let button = document.getElementById('darkModeButton');
   
      // Enable or disable dark mode on or off//
      if (element.classList.contains("dark-mode")) {
