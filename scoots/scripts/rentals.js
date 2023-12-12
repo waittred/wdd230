0edd0812f140a2d41b14f92ab9fef2c4536ca408
waittred <boy15009@byui.edu>
2023-12-12 01:46:25 -0800

    scoots edits

--- scoots/scripts/rentals.js
+++ scoots/scripts/rentals.js
@@ -1,17 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
-        fetch('data/rentals.json')
-            .then(response => response.json())
-            .then(data => {
-                const tableBody = document.querySelector('#rentals-table tbody');
+    fetch('data/rentals.json')
+        .then(response => response.json())
+        .then(data => {
+            const tableBody = document.querySelector('#rentals-table tbody');
 
-                data.rentals.forEach(rental => {
-                    const row = tableBody.insertRow();
-                    row.insertCell(0).textContent = rental.type;
-                    row.insertCell(1).textContent = rental.maxPersons;
-                    row.insertCell(2).textContent = `$${rental.halfDayPrice}`;
-                    row.insertCell(3).textContent = `$${rental.fullDayPrice}`;
-                });
-            })
-            .catch(error => console.error('Error fetching rental data:', error));
-    });
+            data.rentals.forEach(rental => {
+                const row = tableBody.insertRow();
 
+                // Create a cell for the rental type and image
+                const cell = row.insertCell(0);
+
+                // Append the rental type text
+                const rentalTypeText = document.createTextNode(rental.type);
+                cell.appendChild(rentalTypeText);
+
+                // Create an image element and set its source
+                const image = document.createElement('img');
+                image.src = rental.imageUrl;
+                image.alt = rental.type;
+                cell.appendChild(image);
+
+                // Add other cells for additional rental information
+                row.insertCell(1).textContent = rental.maxPersons;
+                row.insertCell(2).textContent = `$${rental.halfDayPrice}`;
+                row.insertCell(3).textContent = `$${rental.fullDayPrice}`;
+            });
+        })
+        .catch(error => console.error('Error fetching rental data:', error));
+});
